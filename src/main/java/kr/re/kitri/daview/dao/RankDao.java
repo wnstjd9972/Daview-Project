8571be792f8dd7221715580ee874e676bf8790ca
Bong0309 <lkb573@gmail.com>
2017-08-03 17:56:03 +0900

    commit 170803 Rank service develop!!! UI working....

--- src/main/java/kr/re/kitri/daview/dao/RankDao.java
+++ src/main/java/kr/re/kitri/daview/dao/RankDao.java
@@ -1,11 +1,9 @@
 package kr.re.kitri.daview.dao;
 
+import kr.re.kitri.daview.InsertService;
 import kr.re.kitri.daview.model.Rank;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
+import org.springframework.stereotype.Repository;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,47 +11,46 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class RankDao {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    //RankService rankService;
+    InsertService insertService;
 
-    private DataSource dataSource;
+    public List<Rank> getRankFestival() {
+        String query = "SELECT title, address, popularity, st_date, end_date FROM festival LIMIT 10";
 
-    public List<Rank> selectTopTenFestival() {
-        String query = "SELECT title, st_date, end_date, address, popularity FROM festival LIMIT 10";
+        //return jdbcTemplate.query(query, new BeanPropertyRowMapper<Rank>(Rank.class));
 
-        return jdbcTemplate.query(query, new BeanPropertyRowMapper<Rank>(Rank.class));
+        //Connection conn = dataSource.getConnection();
+        Connection conn  = insertService.getConnection();
+
+        List<Rank> rankArrayList = new ArrayList<>();
+        Rank rank;
 
 
-        /*try {
-            Connection conn = dataSource.getConnection();
-
+        try {
             PreparedStatement pstmt = conn.prepareStatement(query);
             ResultSet rs = pstmt.executeQuery();
 
-            List<Rank> list = new ArrayList<>();
-
-            Rank rank;
             while (rs.next()){
                 rank = new Rank();
 
-                rank.setTitle(rs.getString("1"));
-                rank.setAddr1(rs.getString("2"));
-                rank.setReadCount(rs.getInt("3"));
-                rank.setEventStartDate(rs.getInt("4"));
-                rank.setEventEndDate(rs.getInt("5"));
+                rank.setTitle(rs.getString(1));
+                rank.setAddr1(rs.getString(2));
+                rank.setReadCount(rs.getInt(3));
+                rank.setEventStartDate(rs.getInt(4));
+                rank.setEventEndDate(rs.getInt(5));
 
-                list.add(rank);
+                rankArrayList.add(rank);
             }
 
             conn.close();
-
-            return list;
         } catch (SQLException e) {
             e.printStackTrace();
-            return new ArrayList<>();
-        }*/
+        }
+        return rankArrayList;
     }
 
 }
--- src/main/java/kr/re/kitri/daview/service/RankService.java
+++ src/main/java/kr/re/kitri/daview/service/RankService.java
@@ -3,15 +3,17 @@ package kr.re.kitri.daview.service;
 
 import kr.re.kitri.daview.dao.RankDao;
 import kr.re.kitri.daview.model.Rank;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class RankService {
 
-    private RankDao dao;
+    @Autowired
+    private RankDao rankDao;
 
-    public List<Rank> viewRankFestival() {
-        return dao.selectTopTenFestival();
+    public List<Rank> getRank() {
+        return rankDao.getRankFestival();
     }
 }
