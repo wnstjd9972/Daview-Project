e7dd84c22c69ca982a390cab6cddaf75de78e020
JunSeongLim <wnstjd9972@gmail.com>
2017-08-08 18:00:20 +0900

    페스티벌 리스트 1차

---
+++ src/main/resources/static/js/FestivalList.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-room-search").onclick = function () { getFestivalList() };
+function getFestivalList(item) {
+    var itemList =  document.getElementById("itemList");
+
+
+
+    itemList.appendChild()
+}
