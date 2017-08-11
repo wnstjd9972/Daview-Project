cd2ecb3d964f6e9be0414b2dfb6da2dfa7371086
JunSeongLim <wnstjd9972@gmail.com>
2017-08-11 16:56:46 +0900

    페스티벌 검색어 입력받기

--- src/main/resources/static/js/FestivalList.js
+++ src/main/resources/static/js/FestivalList.js
@@ -3,14 +3,15 @@ function getFestList() {
     var content = document.getElementsByName("search_fest").item("0").value;
     var workedList = [];
     //match = 포함되어있는
-    for (var i = 0; i < item.length; i++){
-        if(item[i].addr1.match(content) || item[i].title.match(content)) workedList.push(item[i])
+    for (var i = 0; i < item.length; i++) {
+        if (item[i].addr1.match(content) || item[i].title.match(content)) workedList.push(item[i])
 
     }
-    console.log("h")
-};
+    return makeFestList(workedList);
+}
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     $("input[name=search_fest]").keydown(function (key) {
 
         if (key.keyCode == 13) {//키가 13이면 실행 (엔터는 13)
@@ -19,3 +20,26 @@ $(document).ready(function() {
 
     });
 });
+
+
+function makeFestList(item) {
+    var ri = document.getElementById("itemList");
+
+    //새로운 검색시 기존 데이터를 삭제
+    while (ri.hasChildNodes()){
+        ri.removeChild(ri.lastChild)
+    }
+
+    for (var i = 0; i < item.length; i++) {
+        //Div 태그 만들기
+        var iDiv = document.createElement('div');
+        var iImg = document.createElement('img');
+        iImg.src += item[i].firstImage;
+        //추가한 Div태그 itemList 자손으로 넣기
+        var list = document.getElementById("itemList").appendChild(iDiv);
+        //클래스를 list-item으로 설정
+        list.className += "list-item";
+        //list 의 자손을 iImg로 가져옴
+        list.appendChild(iImg);
+    }
+}
