d83bf3cc4660c94fe517e1f4d3ec2b1b3e2ad0e3
gooner573 <lkb573@gmail.com>
2017-08-11 15:18:16 +0900

    170811 commit Rank & NAVER Log-in .....ing...

--- src/main/java/kr/re/kitri/daview/service/RankService.java
+++ src/main/java/kr/re/kitri/daview/service/RankService.java
@@ -1,10 +1,15 @@
 package kr.re.kitri.daview.service;
 
 
+import kr.re.kitri.daview.InsertData;
 import kr.re.kitri.daview.dao.RankDao;
 import kr.re.kitri.daview.model.Rank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -15,5 +20,29 @@ public class RankService {
 
     public List<Rank> getRank() {
         return rankDao.getRankFestival();
+    }
+
+
+    public Connection getConnection() {
+        Connection conn = null;
+        /*try {
+            Class.forName(InsertData.DRIVER_MYSQL).newInstance();
+            System.out.println("driver loading ok..");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }*/
+
+        try {
+            conn = DriverManager.getConnection
+                    (InsertData.DB_URL, InsertData.USERNAME, InsertData.PASSWORD);
+            System.out.println("FestivalDB Connection ok..");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 }
