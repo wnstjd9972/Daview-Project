4431fb7be1ca1e271dd8bb60a6b1b81f0c69bb59
danawacomputer <vnfflvdl1@gmail.com>
2017-08-22 18:37:46 +0900

    q

--- src/main/resources/static/js/SeasonView.js
+++ src/main/resources/static/js/SeasonView.js
@@ -1,71 +1,70 @@
-//봄 3~5월
-function springFestival(item) {
-    var springFest = [];
-    for (var i = 0; i < item.length; i++) {
-        var sday = new Date(item[i].eventStartDate);
-        var eday = new Date(item[i].eventEndDate);
-
-        if ((sday.getMonth() >= 2 && sday.getMonth() <= 4 ) || ( eday.getMonth() >= 2 && eday.getMonth() <= 4)) {
-            // console.log(sday.getMonth());
-            // console.log(eday.getMonth());
-            springFest.push(item[i]);
-        }
-    }
-    return FestivalView(springFest);
-}
-
-//여름은 6~8월
-function summerFestival(item) {
-    var summerFest = [];
-    for (var i = 0; i < item.length; i++) {
-        var sday = new Date(item[i].eventStartDate);
-        var eday = new Date(item[i].eventEndDate);
-
-        if ((sday.getMonth() >= 5 && sday.getMonth() <= 7 ) || ( eday.getMonth() >= 5 && eday.getMonth() <= 7)) {
-            // console.log(sday.getMonth());
-            // console.log(eday.getMonth());
-            summerFest.push(item[i]);
-        }
-    }
-    return FestivalView(summerFest);
-}
-
-//가을은 9~10월
-function fallFestival(item) {
-    var fallFest = [];
-    for (var i = 0; i < item.length; i++) {
-        var sday = new Date(item[i].eventStartDate);
-        var eday = new Date(item[i].eventEndDate);
-
-        if ((sday.getMonth() >= 8 && sday.getMonth() <= 9 ) || ( eday.getMonth() >= 8 && eday.getMonth() <= 9)) {
-            fallFest.push(item[i]);
-            // console.log(sday.getMonth());
-            // console.log(eday.getMonth());
-        }
-    }
-    return FestivalView(fallFest);
-}
-
-//겨울은 11~2월
-function winterFestival(item) {
-    var winterFest = [];
-    for (var i = 0; i < item.length; i++) {
-        var sday = new Date(item[i].eventStartDate);
-        var eday = new Date(item[i].eventEndDate);
-
-        if (((sday.getMonth() >= 10 && sday.getMonth() <= 11 ) || (sday.getMonth() >= 0 && sday.getMonth() <= 1 ))
-            || ((eday.getMonth() >= 10 && eday.getMonth() <= 11) || (eday.getMonth() >= 0 && eday.getMonth() <= 1 ))) {
-            winterFest.push(item[i]);
-            // console.log(sday.getMonth());
-            // console.log(eday.getMonth());
-        }
-    }
-    return FestivalView(winterFest);
-}
+// //봄 3~5월
+// function springFestival(item) {
+//     var springFest = [];
+//     for (var i = 0; i < item.length; i++) {
+//         var sday = new Date(item[i].eventStartDate);
+//         var eday = new Date(item[i].eventEndDate);
+//
+//         if ((sday.getMonth() >= 2 && sday.getMonth() <= 4 ) || ( eday.getMonth() >= 2 && eday.getMonth() <= 4)) {
+//             // console.log(sday.getMonth());
+//             // console.log(eday.getMonth());
+//             springFest.push(item[i]);
+//         }
+//     }
+//     return FestivalView(springFest);
+// }
+//
+// //여름은 6~8월
+// function summerFestival(item) {
+//     var summerFest = [];
+//     for (var i = 0; i < item.length; i++) {
+//         var sday = new Date(item[i].eventStartDate);
+//         var eday = new Date(item[i].eventEndDate);
+//
+//         if ((sday.getMonth() >= 5 && sday.getMonth() <= 7 ) || ( eday.getMonth() >= 5 && eday.getMonth() <= 7)) {
+//             // console.log(sday.getMonth());
+//             // console.log(eday.getMonth());
+//             summerFest.push(item[i]);
+//         }
+//     }
+//     return FestivalView(summerFest);
+// }
+//
+// //가을은 9~10월
+// function fallFestival(item) {
+//     var fallFest = [];
+//     for (var i = 0; i < item.length; i++) {
+//         var sday = new Date(item[i].eventStartDate);
+//         var eday = new Date(item[i].eventEndDate);
+//
+//         if ((sday.getMonth() >= 8 && sday.getMonth() <= 9 ) || ( eday.getMonth() >= 8 && eday.getMonth() <= 9)) {
+//             fallFest.push(item[i]);
+//             // console.log(sday.getMonth());
+//             // console.log(eday.getMonth());
+//         }
+//     }
+//     return FestivalView(fallFest);
+// }
+//
+// //겨울은 11~2월
+// function winterFestival(item) {
+//     var winterFest = [];
+//     for (var i = 0; i < item.length; i++) {
+//         var sday = new Date(item[i].eventStartDate);
+//         var eday = new Date(item[i].eventEndDate);
+//
+//         if (((sday.getMonth() >= 10 && sday.getMonth() <= 11 ) || (sday.getMonth() >= 0 && sday.getMonth() <= 1 ))
+//             || ((eday.getMonth() >= 10 && eday.getMonth() <= 11) || (eday.getMonth() >= 0 && eday.getMonth() <= 1 ))) {
+//             winterFest.push(item[i]);
+//             // console.log(sday.getMonth());
+//             // console.log(eday.getMonth());
+//         }
+//     }
+//     return FestivalView(winterFest);
+// }
 
 
 function FestivalView(item) {
-
     //태그속성주기
     var div = document.createElement('div');
     div.className += "tg-container";
@@ -74,8 +73,8 @@ function FestivalView(item) {
     ul.className += "tg-gallery";
 
 
-    var fallGalDiv = document.getElementById("gallery").appendChild(div);
-    var fallGalUl = fallGalDiv.appendChild(ul);
+    var galDiv = document.getElementById("gallery").appendChild(div);
+    var galUl = galDiv.appendChild(ul);
 
 
     for (var i = 0; i < item.length; i++) {
@@ -97,40 +96,49 @@ function FestivalView(item) {
         var recomImg = document.createElement('img');
         recomImg.src += "/image/season/custom_233.gif";
 
-        var fallGalLiA1 = fallGalUl.appendChild(li);
+        var galLiA1 = galUl.appendChild(li);
 
-        var fallGalLiA2 = fallGalLiA1.appendChild(a);
-        fallGalLiA2.append(img);
-        fallGalLiA2.append("좋아요 " + parseInt(item[i].readCount / 1000) + "개");
+        var galLiA2 = galLiA1.appendChild(a);
+        galLiA2.append(img);
 
-        var fallGalLiA3 = fallGalLiA1.appendChild(a3);
-
-
-        if((item[i].readCount / 1000) < 1) {
-            item[i].readCount = 1000;
+        // var a2 = document.createElement('a');
+        // a2.className += 'like-button';
+        // a2.href += '#';
+        // event 발생시켜야 되는데!!
+        if ((item[i].readCount / 1000) < 1) {
+            item[i].readCount += 1000;
         }
 
 
+        var button = document.createElement('button');
+        button.className += 'like-button';
+        button.id += 'like-button';
+        button.name += item[i].readCount;
+        button.content +=
+            button.onclick = function () {
+                var x = this.innerHTML
+                var y = parseInt(x.replace(/[^0-9]/g, "")) + 1;
+                this.innerHTML = "♡ 좋아요 " + y + "개";
+            };
+
+        var like = galLiA1.appendChild(button);
+
+
+        like.append("♡ 좋아요 " + parseInt(item[i].readCount / 1000) + "개");
+
+
+        var galLiA3 = galLiA1.appendChild(a3);
 
         //best태그
         if (item[i].readCount > 100000) {
-
-            fallGalLiA3.append(recomImg);
-            fallGalLiA3.append(item[i].title);
-
+            galLiA3.append(recomImg);
+            galLiA3.append(item[i].title);
         }
         else {
-
-            fallGalLiA3.append(item[i].title);
+            galLiA3.append(item[i].title);
         }
 
-
-
     }
-
-
-
-
 }
 
 
