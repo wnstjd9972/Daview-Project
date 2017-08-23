8e8af3180b5156d4aef07f8244947633bffa4fec
JunSeongLim <wnstjd9972@gmail.com>
2017-08-23 15:49:27 +0900

    메인 초기화 및 마커 정보표시 구현 끝

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -277,40 +277,13 @@ function getFestivalKeyword() {
     //검색창에서 검색어를 받아옵니다.
     var content = document.getElementsByName("search_fest").item("0").value;
     var workedList = [];
-    console.log(content);
-
     for (var i = 0; i < item.length; i++) {
         if (item[i].addr1.match(content) || item[i].title.match(content)) workedList.push(item[i])
     }
     if (workedList.length == 0) return alert("검색하신 축제가 존재하지 않습니다.");
 
-    $(function () {
-
-        $("#slider-range").slider({
-            range: true,
-            min: new Date('2015.01.01').getTime() / 1000,
-            max: new Date('2018.12.31').getTime() / 1000,
-            step: 86400,
-            values: [new Date('2015.01.01').getTime() / 10000, new Date('2018.12.31').getTime() / 100],
-            slide: function (event, ui) {
-                $("#amount").val(addMonth((new Date(ui.values[0] * 1000))) +
-                    " - " + addMonth((new Date(ui.values[1] * 1000))));
-                USD = new Date(ui.values[0] * 1000);
-                UED = new Date(ui.values[1] * 1000);
-
                 getUserDate();
 
-            }
-        });
-
-
-        $("#amount").val(addMonth((new Date($("#slider-range").slider("values", 0) * 1000))) +
-            " - " + addMonth((new Date($("#slider-range").slider("values", 1) * 1000))));
-    });
-
-    $('select').find('option:first').attr('selected', 'selected');
-    city = '';
-    return getMainMap(workedList)
 }
 
 //검색창에 엔터가 눌러지면 getFestivalKeyword 함수가 실행됩니다.
@@ -414,4 +387,34 @@ function userCheckDate(item) {
 function selectCity(selectObj) {
     city = selectObj.value;
     return getUserDate();
+}
+
+
+function reset() {
+    $('select').find('option:first').attr('selected', 'selected');
+    city = '';
+    document.getElementsByName("search_fest").item("0").value = '';
+    USD = new Date();
+    UED = new Date();
+    $(function () {
+        $("#slider-range").slider({
+            range: true,
+            min: new Date('2015.01.01').getTime() / 1000,
+            max: new Date('2018.12.31').getTime() / 1000,
+            step: 1,
+            values: [new Date().getTime() / 1000, new Date().getTime() / 1000],
+            slide: function (event, ui) {
+                $("#amount").val(addMonth((new Date(ui.values[0] * 1000))) +
+                    " - " + addMonth((new Date(ui.values[1] * 1000))));
+                USD = new Date(ui.values[0] * 1000);
+                UED = new Date(ui.values[1] * 1000);
+                getUserDate()
+            }
+        });
+        $("#amount").val(addMonth((new Date($("#slider-range").slider("values", 0) * 1000))) +
+            " - " + addMonth((new Date($("#slider-range").slider("values", 1) * 1000))));
+    });
+
+    getMainMap(ingFestival(item))
+
 }
